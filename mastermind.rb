496b893ad8a96ab0a0691690d239d27628b3984d
Bruce Cong <brucejcong@gmail.com>
2018-09-21 14:16:54 -0700

    Refactored code to better suit codebreaker and codemaster format.

--- mastermind.rb
+++ mastermind.rb
@@ -10,21 +10,25 @@ class Mastermind
 
 	def play
 		puts "Yo, what's your name?"
-		player_name = gets.chomp
+		player = gets.chomp
 		puts "Would you like to be codemaster. Yes or no?"
 		if gets.chomp.downcase.include?("yes")
-			@codemaster = Codemaster.new(player_name, @@code_colors)
-			@codebreaker = Codebreaker.new("computer", @@code_colors)
+			@codemaster = Codemaster.new(player, @@code_colors)
+			@codebreaker = Codebreaker.new(:computer, @@code_colors)
 		else
-			@codebreaker = Codebreaker.new(player_name, @@code_colors)
-			@codemaster = Codemaster.new("computer", @@code_colors)
+			@codebreaker = Codebreaker.new(player, @@code_colors)
+			@codemaster = Codemaster.new(:computer, @@code_colors)
 		end
 
 		until @turns == 0 
-			puts "You have #{@turns} to guess the code. Please enter 4 colors separated by comma. Here's the list of colors: " 
+			print "You have #{@turns} turns to guess the code. "
+			puts "Please enter 4 colors separated by comma. Here's the list of colors: "
 			puts @@code_colors.join(", ")
 			guess = @codebreaker.guess
-
+			until valid?(guess)
+				puts "Please enter a valid guess. Example: 'Blue, Green, Purple, Yellow'."
+				guess = @codebreaker.guess
+			end
 			if @codemaster.check_guess(guess).nil?
 				@turns -= 1
 			else
@@ -33,47 +37,81 @@ class Mastermind
 			end
 		end
 		@winner = @codemaster if @winner.nil?
-		puts "#{@winner.player_name.capitalize} wins!"
+		puts "#{@winner.player.capitalize} wins!"
 	end
 
 
+	def valid?(guess)
+		return false if guess.length != 4
+		guess.each do |color|
+			if !@@code_colors.include?(color.capitalize)
+				return false
+			end
+		end
+		return true
+	end
+
+
+
+	# def code
+	# 	puts "Please enter 4 colors separated by comma. Here's the list of colors: "
+	# 	puts @@code_colors.join(", ")
+	# 	guess = gets.chomp.split(/\s*,\s*/)
+	# 	until valid?(guess)
+	# 		puts "Please enter a valid guess. Example: 'Blue, Green, Purple, Yellow'."
+	# 		guess = gets.chomp.split(/\s*,\s*/)
+	# 	end
+	# 	guess
+
+	# end
+
 	class Codebreaker
-		attr_reader :player_name
-		def initialize(player_name, code_colors)
-			@player_name = player_name
+		attr_reader :player
+		def initialize(player, code_colors)
+			@player = player
 			@code_colors = code_colors
 		end
 
 		def guess
-			guess = gets.chomp.split(/\s*,\s*/)
-			until valid?(guess)
-				puts "Please enter a valid guess. Example: 'Blue, Green, Purple, Yellow'."
+			if player != :computer
 				guess = gets.chomp.split(/\s*,\s*/)
 			end
-			guess
 		end
 
-		def valid?(guess)
-			return false if guess.length != 4
-			guess.each do |color|
-				if !@code_colors.include?(color.capitalize)
-					return false
-				end
-			end
-			return true
-		end
+		# def valid?(guess)
+		# 	return false if guess.length != 4
+		# 	guess.each do |color|
+		# 		if !@code_colors.include?(color.capitalize)
+		# 			return false
+		# 		end
+		# 	end
+		# 	return true
+		# end
 
 	end
 
 	class Codemaster 
-		attr_reader :answer, :player_name
-		def initialize(player_name, code_colors)
+		attr_reader :answer, :player
+		def initialize(player, code_colors)
 			@answer = Array.new()
-			@player_name = player_name
-			4.times do 
-				@answer << code_colors[rand(6)]
+			@player = player
+			@code_colors = code_colors
+			# if @player == :computer
+			# 	4.times do 
+			# 		@answer << code_colors[rand(6)]
+			# 	end
+			# end
+			create_code
+		end
+
+		def create_code
+			if @player == :computer
+				4.times do 
+					@answer << @code_colors[rand(6)]
+				end
 			end
 		end
+
 
 		def check_guess(guess)
 			color_counts = Hash.new(0)
